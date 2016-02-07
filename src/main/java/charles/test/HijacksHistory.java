c7c4db3f6c9f3a10555a401cb154a2073dafa948
Charles Brock (Laptop) <jbrock@nmt.edu>
2016-02-07 11:59:05 -0800

    This should increase the number of suspicious ASes found from 0.

--- src/main/java/charles/test/HijacksHistory.java
+++ src/main/java/charles/test/HijacksHistory.java
@@ -11,66 +11,76 @@ public class HijacksHistory
 {
     final double threshold = .01;
     final int prefixThreshold = 8;
-    
+
     private Map<Prefix, Double> totalTimeMap = new HashMap<>();
-    
+
     // map of prefix to map of as to time at AS
     private Map<Prefix, Map<String, Long>> prefixHostHistory = new HashMap<>();
-    
+
     // map of thread to map of prefix to last announcement time
     private Map<Thread, Map<Prefix, Long>> lastAnnouncementByThread = new HashMap<>();
-    
+
     // a map of as to announced prefixes
     private Map<String, Set<Prefix>> asGuestHistory = new HashMap<>();
-    
+
     synchronized public boolean isSuspiciousAS(String AS)
     {
 	Set<Prefix> prefixes = asGuestHistory.get(AS);
-	
-	if(prefixes == null || prefixes.size() < 2)
+
+	if (prefixes == null || prefixes.size() < 2)
 	    return false; // trust the little guys... as long as they are little
-	
+
 	double conflictingAsScore = 0;
-	
-	for(Prefix prefix : prefixes)
+
+	for (Prefix prefix : prefixes)
 	{
 	    Map<String, Long> prefixHosts = prefixHostHistory.get(prefix);
-	    
-	    if(prefixHosts == null)
-		continue; // ignore prefixes if they are brand new
-	    
+
+	    if (prefixHosts == null)
+	    {
+		int steps = 32;
+		for (Entry<Prefix, Map<String, Long>> entry : prefixHostHistory.entrySet())
+		{
+		    if (entry.getKey().isSubset(prefix) && entry.getKey().prefix - prefix.prefix < steps)
+		    {
+			steps = entry.getKey().prefix - prefix.prefix;
+			prefixHosts = entry.getValue();
+		    }
+		}
+	    }
+
 	    conflictingAsScore += 1 - (1.0 / prefixHosts.size());
 	}
-	
+
 	conflictingAsScore /= prefixes.size();
-	
-	return conflictingAsScore > .5;
+
+	return conflictingAsScore > .25;
     }
-    
+
     synchronized boolean isAnnouncementGood(Prefix prefix, String AS, long time)
     {
-	if(prefix.prefix <= prefixThreshold)
+	if (prefix.prefix <= prefixThreshold)
 	    return true; // we don't care about the defaultish routes
-	
-	if(!asGuestHistory.containsKey(AS))
+
+	if (!asGuestHistory.containsKey(AS))
 	    asGuestHistory.put(AS, new HashSet<Prefix>());
 	asGuestHistory.get(AS).add(prefix);
-	
+
 	Map<Prefix, Long> lastAnnouncement = lastAnnouncementByThread.get(Thread.currentThread());
-	if(lastAnnouncement == null)
+	if (lastAnnouncement == null)
 	{
 	    lastAnnouncement = new HashMap<>();
 	    lastAnnouncementByThread.put(Thread.currentThread(), lastAnnouncement);
 	}
-	
+
 	Long lastTimeSeen = lastAnnouncement.get(prefix);
-	
-	if(lastTimeSeen == null)
+
+	if (lastTimeSeen == null)
 	{
 	    int steps = 32;
-	    for(Entry<Prefix, Long> entry : lastAnnouncement.entrySet())
+	    for (Entry<Prefix, Long> entry : lastAnnouncement.entrySet())
 	    {
-		if(entry.getKey().isSubset(prefix) && entry.getKey().prefix - prefix.prefix < steps)
+		if (entry.getKey().isSubset(prefix) && entry.getKey().prefix - prefix.prefix < steps)
 		{
 		    steps = entry.getKey().prefix - prefix.prefix;
 		    lastTimeSeen = entry.getValue();
@@ -78,36 +88,36 @@ public class HijacksHistory
 	    }
 	}
 	lastAnnouncement.put(prefix, time);
-	if(lastTimeSeen == null)
+	if (lastTimeSeen == null)
 	    return true;
 	time -= lastTimeSeen;
-	
-	if(!prefixHostHistory.containsKey(prefix))
+
+	if (!prefixHostHistory.containsKey(prefix))
 	{
 	    prefixHostHistory.put(prefix, new HashMap<String, Long>());
 	}
 	Long timeAtAS = prefixHostHistory.get(prefix).get(AS);
-	if(timeAtAS == null)
+	if (timeAtAS == null)
 	    timeAtAS = (long) 0;
 	timeAtAS += time;
 	prefixHostHistory.get(prefix).put(AS, timeAtAS);
-	
+
 	double totalTime = 0;
-	for(Entry<Prefix, Double> entry : totalTimeMap.entrySet())
+	for (Entry<Prefix, Double> entry : totalTimeMap.entrySet())
 	{
-	    if(entry.getKey().isSubset(prefix))
+	    if (entry.getKey().isSubset(prefix))
 	    {
 		totalTime = Math.max(totalTime, entry.getValue());
 	    }
 	}
-	
+
 	return timeAtAS / totalTime > threshold;
     }
-    
+
     synchronized public double getTotalTimeSum()
     {
 	double totalTime = 0;
-	for(Entry<Prefix, Double> entry : totalTimeMap.entrySet())
+	for (Entry<Prefix, Double> entry : totalTimeMap.entrySet())
 	{
 	    totalTime += entry.getValue();
 	}
