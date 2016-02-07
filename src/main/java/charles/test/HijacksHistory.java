46eef6dfc12c98a88bd9216787a4a0c4e73069b8
Charles Brock (Laptop) <jbrock@nmt.edu>
2016-02-07 14:24:04 -0800

    This might go faster.

--- src/main/java/charles/test/HijacksHistory.java
+++ src/main/java/charles/test/HijacksHistory.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 // This class would be much better if it kept the state of the internet and an assumed view of it from each AS
 public class HijacksHistory
@@ -16,72 +17,82 @@ public class HijacksHistory
     private Map<Prefix, Double> totalTimeMap = new HashMap<>();
 
     // map of prefix to map of as to time at AS
-    private Map<Prefix, Map<String, Long>> prefixHostHistory = new HashMap<>();
+    private ConcurrentHashMap<Prefix, ConcurrentHashMap<String, Long>> prefixHostHistory = new ConcurrentHashMap<>();
 
     // map of thread to map of prefix to last announcement time
-    private Map<Thread, Map<Prefix, Long>> lastAnnouncementByThread = new HashMap<>();
+    private ConcurrentHashMap<Thread, ConcurrentHashMap<Prefix, Long>> lastAnnouncementByThread = new ConcurrentHashMap<>();
 
     // a map of as to announced prefixes
     private Map<String, Set<Prefix>> asGuestHistory = new HashMap<>();
 
-    
     private double maxObservedAsScore = 0;
-    
-    synchronized public boolean isSuspiciousAS(String AS)
+
+    public boolean isSuspiciousAS(String AS)
     {
-	Set<Prefix> prefixes = asGuestHistory.get(AS);
+	Set<Prefix> prefixes;
+	synchronized (asGuestHistory)
+	{
+	    prefixes = asGuestHistory.get(AS);
+	}
 
 	if (prefixes == null || prefixes.size() < 2)
 	    return false; // trust the little guys... as long as they are little
 
 	double conflictingAsScore = 0;
 
-	for (Prefix prefix : prefixes)
+	synchronized (asGuestHistory)
 	{
-	    Map<String, Long> prefixHosts = prefixHostHistory.get(prefix);
-
-	    if (prefixHosts == null)
+	    for (Prefix prefix : prefixes)
 	    {
-		int steps = 32;
-		for (Entry<Prefix, Map<String, Long>> entry : prefixHostHistory.entrySet())
+		Map<String, Long> prefixHosts = prefixHostHistory.get(prefix);
+
+		if (prefixHosts == null)
 		{
-		    if (entry.getKey().isSubset(prefix) && entry.getKey().prefix - prefix.prefix < steps)
+		    int steps = 32;
+		    for (Entry<Prefix, ConcurrentHashMap<String, Long>> entry : prefixHostHistory.entrySet())
 		    {
-			steps = entry.getKey().prefix - prefix.prefix;
-			prefixHosts = entry.getValue();
+			if (entry.getKey().isSubset(prefix) && entry.getKey().prefix - prefix.prefix < steps)
+			{
+			    steps = entry.getKey().prefix - prefix.prefix;
+			    prefixHosts = entry.getValue();
+			}
 		    }
 		}
-	    }
-	    
-	    if(prefixHosts == null)
-		continue; // why do I need this?
 
-	    conflictingAsScore += 1 - (1.0 / prefixHosts.size());
+		if (prefixHosts == null)
+		    continue; // why do I need this?
+
+		conflictingAsScore += 1 - (1.0 / prefixHosts.size());
+	    }
+
+	    conflictingAsScore /= prefixes.size();
 	}
 
-	conflictingAsScore /= prefixes.size();
-
-	if(conflictingAsScore > maxObservedAsScore)
+	if (conflictingAsScore > maxObservedAsScore)
 	    maxObservedAsScore = conflictingAsScore;
-	if(conflictingAsScore > .9 * maxObservedAsScore)
-	    System.out.println(AS + " got a suspicion score of " + conflictingAsScore + " (Max Score So Far: " + maxObservedAsScore + ")");
-	
+	if (conflictingAsScore > .9 * maxObservedAsScore)
+	    System.out.println(AS + " got a suspicion score of " + conflictingAsScore + " (Max Score So Far: "
+		    + maxObservedAsScore + ")");
+
 	return conflictingAsScore > suspiciousAsThreshold;
     }
 
-    synchronized boolean isAnnouncementGood(Prefix prefix, String AS, long time)
+    boolean isAnnouncementGood(Prefix prefix, String AS, long time)
     {
 	if (prefix.prefix <= prefixThreshold)
 	    return true; // we don't care about the defaultish routes
 
-	if (!asGuestHistory.containsKey(AS))
-	    asGuestHistory.put(AS, new HashSet<Prefix>());
-	asGuestHistory.get(AS).add(prefix);
+	synchronized (asGuestHistory)
+	{
+	    if (!asGuestHistory.containsKey(AS))
+		asGuestHistory.put(AS, new HashSet<Prefix>());
+	    asGuestHistory.get(AS).add(prefix);
+	}
 
-	Map<Prefix, Long> lastAnnouncement = lastAnnouncementByThread.get(Thread.currentThread());
+	ConcurrentHashMap<Prefix, Long> lastAnnouncement = lastAnnouncementByThread.get(Thread.currentThread());
 	if (lastAnnouncement == null)
 	{
-	    lastAnnouncement = new HashMap<>();
+	    lastAnnouncement = new ConcurrentHashMap<>();
 	    lastAnnouncementByThread.put(Thread.currentThread(), lastAnnouncement);
 	}
 
@@ -106,7 +117,7 @@ public class HijacksHistory
 
 	if (!prefixHostHistory.containsKey(prefix))
 	{
-	    prefixHostHistory.put(prefix, new HashMap<String, Long>());
+	    prefixHostHistory.put(prefix, new ConcurrentHashMap<String, Long>());
 	}
 	Long timeAtAS = prefixHostHistory.get(prefix).get(AS);
 	if (timeAtAS == null)
@@ -128,7 +139,7 @@ public class HijacksHistory
 	return timeAtAS / totalTime > threshold;
     }
 
-    synchronized public double getTotalTimeSum()
+    public double getTotalTimeSum()
     {
 	double totalTime = 0;
 	for (Entry<Prefix, Double> entry : totalTimeMap.entrySet())
